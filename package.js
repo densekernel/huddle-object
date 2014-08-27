ac92357723cd6498ddd25564914772dd7a089502
jonnymanf <me@jonnymanfield.com>
2014-08-27 16:08:07 +0100

    vc

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     summary: "HuddleObject - Add HTML div elements to HuddleCanvas with support for multi-touch gestures",
     version: "0.2.4",
-    name: "j5j5:huddleobject",
+    name: "jay5:huddleobject",
     git: 'https://github.com/jonnymanf/HuddleObject.git'
 });
 
 Package.on_use(function(api) {
-    api.use('jquery@1.11.0', 'client');
-    api.imply('jquery@1.11.0', 'client');
+    api.use('jquery@1.0.0', 'client');
+    api.imply('jquery@1.0.0', 'client');
     api.add_files([
         'huddleobject.js',
         'huddleobject.css'
